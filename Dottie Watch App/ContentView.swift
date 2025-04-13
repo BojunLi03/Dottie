c807f7607b32ff5b97728f06e52467450b331093
Abby Chen <archen@andrew.cmu.edu>
2025-04-13 18:06:40 -0400

    added logo and welcome text

--- Dottie Watch App/ContentView.swift
+++ Dottie Watch App/ContentView.swift
@@ -12,29 +12,46 @@ struct ContentView: View {
         // Wrap the main VStack in a NavigationView
         NavigationView {
             VStack(spacing: 20) {
-                // Button to connect to glucose monitor
-                Button(action: {
-                    connectToGlucoseMonitor()
-                }) {
-                    Text("Connect to Glucose Monitor")
-                        .font(.headline)
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
-                
-                // NavigationLink to GlucoseGraphView
-                NavigationLink(destination: GlucoseGraphView(glucoseDataManager: GlucoseDataManager())) {
-                    Text("View Glucose Levels")
-                        .font(.headline)
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.green)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
+              Spacer()
+              
+              // Dottie logo
+              Image("dottie_logo")
+                  .resizable()
+                  .aspectRatio(contentMode: .fit)
+                  .frame(width: 80, height: 80)
+                  .cornerRadius(15)
+                  .padding(.bottom, 1)
+              
+              // Welcome text
+              Text("Welcome to Dottie!")
+                  .font(.title3)
+                  .fontWeight(.semibold)
+                  .foregroundColor(.white)
+                  .padding(.bottom, 3)
+              
+              // NavigationLink to GlucoseGraphView
+              NavigationLink(destination: GlucoseGraphView(glucoseDataManager: GlucoseDataManager())) {
+                  Text("View Glucose Levels")
+                      .font(.headline)
+                      .frame(maxWidth: .infinity)
+                      .padding()
+                      .background(Color.green)
+                      .foregroundColor(.white)
+                      .cornerRadius(10)
+              }
+              
+              // Button to view history
+              Button(action: {
+                  print("View History")
+              }) {
+                  Text("View History")
+                      .font(.headline)
+                      .frame(maxWidth: .infinity)
+                      .padding()
+                      .background(Color.blue)
+                      .foregroundColor(.white)
+                      .cornerRadius(10)
+              }
             }
             .padding()
             //.navigationTitle("Dottie Watch App") // Optional: Add a title for the navigation bar
